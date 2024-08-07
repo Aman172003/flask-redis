6e383f62635a71b112dbda45991013ae85857774
Aman172003 <tej171995@gmail.com>
2024-08-07 17:44:59 +0530

    changes

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-# app.py
-from flask import Flask, jsonify
-from routes.user import user
+from flask import Flask
+from routes.book_routes import book
 
 app = Flask(__name__)
 
+# Register Blueprints
+app.register_blueprint(book, url_prefix="/books")
+
 @app.route('/', methods=['GET'])
 def hello():
-    return jsonify({"message": "Hello"}), 200
-
-# Register Blueprints
-app.register_blueprint(user, url_prefix="/")
+    return {"message": "Welcome to the Book Management System!"}, 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
